f45df758ed702e82b9169f9498c3f53b5bfe683a
Armstrong1124 <13120566370@163.com>
2019-08-10 11:16:02 +0800

    test

--- helloworld/helloworld.py
+++ helloworld/helloworld.py
@@ -10,8 +10,3 @@ print(small)
 for i in a[2:]:
     small = np.row_stack((small[1:], i))
     print(small)
-
-# DataFrame更改列名
-a = pd.DataFrame(np.zeros((3, 4)), columns=list("ABCD"))
-a.rename(columns={"A": "G"}, inplace=True)
-print(a)
